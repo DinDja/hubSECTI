42e7e89284209474e05662f1695d25d75ea7a70c
Bruno <obitoandradeuthiha@gmail.com>
2026-04-10 22:01:32 -0300

    UP

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -304,7 +304,7 @@ export function AboutSection() {
           </div>
           
           <h2 className="text-4xl md:text-6xl lg:text-7xl font-black tracking-tight leading-[1.1] mb-6">
-            Ciencia e tecnologia
+            Ciencia tecnologia e inovacao
             <br />
             <span className="relative inline-block">
               para a{" "}
